4ebeff532f8c0e3c68fa5f867853b55b5fa0c041
duonvipbn <duonvipbn@gmail.com>
2025-07-03 22:35:11 +0700

    update

---
+++ Session10/Baitap07.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+Node* createNode(int value) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+Node* insertHead(Node* head, int value) {
+    Node* newNode = createNode(value);
+    newNode->next = head;
+    return newNode;
+}
+
+void printList(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        printf("%d->", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+void sortList(Node* head) {
+    Node* i = head;
+    while (i != NULL) {
+        Node* j = i->next;
+        while (j != NULL) {
+            if (i->data > j->data) {
+                int temp = i->data;
+                i->data = j->data;
+                j->data = temp;
+            }
+            j = j->next;
+        }
+        i = i->next;
+    }
+}
+
+int main() {
+    Node* head = NULL;
+    head = insertHead(head, 3);
+    head = insertHead(head, 7);
+    head = insertHead(head, 5);
+
+    printList(head);
+
+    sortList(head);
+
+    printList(head);
+
+    Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    return 0;
+}
